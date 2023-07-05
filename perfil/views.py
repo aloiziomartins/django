9798d1cdc2b2f3bb100cb6c9f7a29123a00d7350
Aloizio Martins <martins@CRI-877242.ad.criciuma.ifsc.edu.br>
2023-07-05 16:58:05 -0300

    Instalação,configuração parte 1 concluída

---
+++ perfil/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def gerenciar(request):
+    return render(request, 'gerenciar.html')
